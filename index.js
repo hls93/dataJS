2bd5bd7aa7135e11f2a0a229af9bd45ff873c02a
hls93 <hlshaw3@ncsu.edu>
2017-07-25 16:43:15 -0400

    adding mustache data

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const data = require('./data.js');
+const data = require('./data');
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
 
+//attach mustache
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+//make public folder usable
 app.use(express.static('public'));
 
-app.get('/:module.exports', function(req, res){
-
-  res.render('home', {
-    user: req.param.module.exports
-  });
+//
+app.get('/', function(req, res){
+  res.render('home', data)
 });
+
+app.get('/:user', function(req, res){
+  req.render('user', data)
+})
+
 
 app.listen(3000, function(){
   console.log('success!');
